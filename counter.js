66322d9263430c56bc3c401fa7b095c84ff8270c
Rajshekar pujari <130205130+RAjshekarPUjarii@users.noreply.github.com>
2024-03-03 23:08:02 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -2,9 +2,9 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 let count = 0;
 btns.forEach(function (btn) {
-    //  added commits
+    
     btn.addEventListener("click", function (e) {
-        // console.log(e.currentTarget.classList)
+    
         const styles = e.currentTarget.classList;
         if (styles.contains("increase")) {
             count++;
